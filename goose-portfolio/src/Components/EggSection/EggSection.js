87774decc35cd3b546996962213e71471daa75ab
veepgoose <victoriaplatt@loveplanes.co.uk>
2023-08-04 12:48:16 +0100

    Egg hatching animation, and button added

--- goose-portfolio/src/Components/EggSection/EggSection.js
+++ goose-portfolio/src/Components/EggSection/EggSection.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './EggSection.css';
-import PinkEgg from './PinkEgg.png';
+import PinkEgg from './GooseLay.gif';
 
 
 function EggSection (props) {
@@ -8,6 +8,7 @@ function EggSection (props) {
       <div className="EggSection">
       <header className={props.className}>
       <img src={PinkEgg} alt="PinkEgg" className="PinkEgg"/>
+      <button className="EggButton">Click to Hatch Me</button>    
      
        
       </header>
