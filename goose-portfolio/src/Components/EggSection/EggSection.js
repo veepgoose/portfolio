aee3cc440139bce677566aec7ed89ea49b1a0ce3
Goose <victoriaplatt@loveplanes.co.uk>
2023-08-05 12:23:31 +0100

    Used onClick and useState to render HatchingEgg upon click of the Hatch Me button - Currently the gif is displaying *beneath* the button rather than above, more work to be done here.

--- goose-portfolio/src/Components/EggSection/EggSection.js
+++ goose-portfolio/src/Components/EggSection/EggSection.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React,  {useState } from 'react';
 import './EggSection.css';
-import PinkEgg from './GooseLay.gif';
+import HatchingEgg from './GooseLay.gif';
 import AboutMe from '../AboutMe/AboutMe.js';
+import UncrackedEgg from './PinkEgg.png';
 
 
 function EggSection (props) {
+    const [isHatched, setIsHatched] = useState(false);
+
+    const HatchEgg = () => {
+        setIsHatched(true);
+    };
+
     return (
       <div className="EggSection">
       <header className={props.className}>
-      <img src={PinkEgg} alt="PinkEgg" className="PinkEgg"/>
-      <button className="EggButton">HATCH ME</button> 
+      <img src={UncrackedEgg} alt="UncrackedEgg" className="UncrackedEgg"/>
+      <button onClick={HatchEgg}  className="EggButton">HATCH ME</button> 
+      {isHatched && <img src={HatchingEgg} alt="HatchingEgg" className="HatchingEgg"/>
+      }
       <AboutMe className="AboutMe"/>   
      
-       
+      
       </header>
       </div>
     );
