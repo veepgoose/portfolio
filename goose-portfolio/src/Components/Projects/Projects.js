530f285794ed48451e75c2e5f5a18b02a882678b
Goose <victoriaplatt@loveplanes.co.uk>
2023-08-06 17:25:34 +0100

    ProjEggs images added to Projects Components and styled into 2 rows

--- goose-portfolio/src/Components/Projects/Projects.js
+++ goose-portfolio/src/Components/Projects/Projects.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import './Projects.css';
 import ProjectsTitle from './Projects.png';
+import ProjEgg from './ProjEgg.png';
 
 
 function Projects (props) {
     return (
         <div className="Projects">
         <header className={props.className}>
-        <img src={ProjectsTitle} alt="ProjectsTitle" className="ProjectsTitle"/>   
+        <div className="ProjectsHeader">
+        <img src={ProjectsTitle} alt="ProjectsTitle" className="ProjectsTitle"/> 
+        </div>
+        <div className="EggCarton">
+            <div className="EggTop">
+                 <img src={ProjEgg} alt="ProjEgg" className="ProjEgg1"/>
+                 <img src={ProjEgg} alt="ProjEgg" className="ProjEgg2"/>
+                 <img src={ProjEgg} alt="ProjEgg" className="ProjEgg3"/>
+            </div>
+            <div className="EggBottom">
+                 <img src={ProjEgg} alt="ProjEgg" className="ProjEgg4"/>
+                 <img src={ProjEgg} alt="ProjEgg" className="ProjEgg5"/>
+                 <img src={ProjEgg} alt="ProjEgg" className="ProjEgg6"/>
+        </div>  
+        </div>
         </header>
         </div>
     );
