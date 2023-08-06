e92087ec00bf6dfb1b04eb8b8a0b2a104a7e0b30
Goose <victoriaplatt@loveplanes.co.uk>
2023-08-06 21:00:39 +0100

    Changed concept and layout of Projects section/component

--- goose-portfolio/src/Components/Projects/Projects.js
+++ goose-portfolio/src/Components/Projects/Projects.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './Projects.css';
 import ProjectsTitle from './Projects.png';
-import ProjEgg from './ProjEgg.png';
+import LottoEgg from './LottoGenEgg.png';
+import GitEggsy from './GitTipsyEgg.png';
+import MemoryEgg from './MemoryCompanionEgg.png';
 
 
 function Projects (props) {
@@ -10,18 +12,14 @@ function Projects (props) {
         <header className={props.className}>
         <div className="ProjectsHeader">
         <img src={ProjectsTitle} alt="ProjectsTitle" className="ProjectsTitle"/> 
+        <p className="ProjectsText">Here are some of my projects! 
+        <br></br>
+        Click on the eggs to learn more about them!</p>
         </div>
         <div className="EggCarton">
-            <div className="EggTop">
-                 <img src={ProjEgg} alt="ProjEgg" className="ProjEgg1"/>
-                 <img src={ProjEgg} alt="ProjEgg" className="ProjEgg2"/>
-                 <img src={ProjEgg} alt="ProjEgg" className="ProjEgg3"/>
-            </div>
-            <div className="EggBottom">
-                 <img src={ProjEgg} alt="ProjEgg" className="ProjEgg4"/>
-                 <img src={ProjEgg} alt="ProjEgg" className="ProjEgg5"/>
-                 <img src={ProjEgg} alt="ProjEgg" className="ProjEgg6"/>
-        </div>  
+            <img src={LottoEgg} alt="LottoEgg" className="ProjEgg"/>
+            <img src={GitEggsy} alt="GitEggsy" className="ProjEgg"/>
+            <img src={MemoryEgg} alt="MemoryEgg" className="ProjEgg"/>
         </div>
         </header>
         </div>
