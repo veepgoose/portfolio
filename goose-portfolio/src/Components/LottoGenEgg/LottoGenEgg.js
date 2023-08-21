dabf651ed7f97410a0146639e807671d77170f51
Goose <victoriaplatt@loveplanes.co.uk>
2023-08-21 09:46:47 +0100

    Styling on egg and octocat, link added to egg, links to deployed app, octocat links to github repo

--- goose-portfolio/src/Components/LottoGenEgg/LottoGenEgg.js
+++ goose-portfolio/src/Components/LottoGenEgg/LottoGenEgg.js
@@ -2,7 +2,8 @@ import React from 'react';
 import './LottoGenEgg.css';
 import LottoTitle from './LotteryNumberGenTitle.png'; 
 import OctoCat from './OctoCat.png';
-import LottoEgg from '../Projects/LottoGenEgg.png';
+import LottoAppEgg from './LottoGenEgg.png';
+import LottoApp from './Lotto.JPG';
 
 
 
@@ -11,8 +12,13 @@ function LottoProj (props) {
       <div className="LottoPage">
       <header className={props.className}>
       <img src={LottoTitle} alt="LottoTitle" className="LottoTitle"/>
-      <img src={LottoEgg} alt="LottoEgg" className="ProjEgg"/>
+      <a href="https://gooselottoapp.vercel.app/" target="_blank" rel="noreferrer">
+      <img src={LottoAppEgg} alt="LottoEgg" className="LottoEgg"/>
+      </a>
+      <a href="https://github.com/veepgoose/GooseLotteryGen" target="_blank" rel="noreferrer">
       <img src={OctoCat} alt="OctoCat" className="OctoCat"/>
+      </a>
+      <img src={LottoApp} alt="LottoApp" className="LottoApp"/>
      
        
       </header>
