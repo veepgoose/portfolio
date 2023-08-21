7b2ea45e768eee07db9c03f00219b5df47c5340b
Goose <victoriaplatt@loveplanes.co.uk>
2023-08-21 08:44:10 +0100

    Fixed issues with routing by restructuring App.js and wrapping within a Fragment. Tested and working!

--- goose-portfolio/src/App.js
+++ goose-portfolio/src/App.js
@@ -8,19 +8,22 @@ import Footer from './Components/Footer/Footer';
 import LottoGenEgg from './Components/LottoGenEgg/LottoGenEgg';
 import TipsyEgg from './Components/TipsyEgg/TipsyEgg';
 import MemCompEgg from './Components/MemCompEgg/MemCompEgg';
+import { Fragment } from 'react';
 
 
 
 function App() {
   return (
+
+  <Fragment>
     <BrowserRouter>
+
     <Routes>
-      <Route path="/LottoGenEgg" element={<LottoGenEgg/>} />
-      <Route path="/TipsyEgg" element={<TipsyEgg />} />
-      <Route path="/MemCompEgg" element={<MemCompEgg />} />
-    </Routes>
-    
-    <div className="App">
+
+      <Route path="/" element={
+        <div>
+         
+          <div className="App">
       <header className="App-header">
         
         <p>
@@ -34,7 +37,19 @@ function App() {
         
       </header>
     </div>
+          
+        </div>
+        } />
+
+      <Route path="/LottoGenEgg" element={<LottoGenEgg/>} />
+      <Route path="/TipsyEgg" element={<TipsyEgg />} />
+      <Route path="/MemCompEgg" element={<MemCompEgg />} />
+    </Routes>
     </BrowserRouter>
+   
+
+  </Fragment>
+    
   );
 }
 
