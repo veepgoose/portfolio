d912d5666aa32a21ad23da026e3dcc260e66ac37
Goose <victoriaplatt@loveplanes.co.uk>
2023-08-29 19:25:47 +0100

    Added buy me a coffee to contacts

--- goose-portfolio/src/Components/Contact/ContactIcons.js
+++ goose-portfolio/src/Components/Contact/ContactIcons.js
@@ -1,13 +1,10 @@
 import instagram from '../Assets/instagram.svg';
 import linkedin from '../Assets/linkedin.svg';
 import email from '../Assets/email.svg';
+import coffee from '../Assets/coffee.svg';
 
 export const contact = [
-    {
-        name: 'Instagram',
-        icon: instagram,
-        link: 'https://www.instagram.com/plattybird/'
-    },
+   
 
     {
         name: 'LinkedIn',
@@ -19,6 +16,18 @@ export const contact = [
         name: 'Email',
         icon: email,
         link: 'mailto: victoriaplatt@loveplanes.co.uk'
+    },
+
+    {
+        name: 'Instagram',
+        icon: instagram,
+        link: 'https://www.instagram.com/plattybird/'
+    },
+
+    {   
+        name: 'Buy me a coffee',
+        icon: coffee,
+        link: 'https://www.buymeacoffee.com/goosesquad'
     }
 ]
 
